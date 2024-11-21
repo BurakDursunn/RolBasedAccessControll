1873ff1e883bf1270c29386279be56cc9089f21d
Burak Dursun <burakdursunn.bd@gmail.com>
2024-11-21 15:56:34 +0300

    commit

---
+++ src/main/java/com/example/authoritymanagementsystem/AuthorityManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.authoritymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityManagementSystemApplication.class, args);
+    }
+
+}
