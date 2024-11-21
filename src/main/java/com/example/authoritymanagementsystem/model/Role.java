31acd6bb8027918f3df2f3908d5b58cdf5c3aeba
Burak Dursun <burakdursunn.bd@gmail.com>
2024-11-21 15:55:56 +0300

    commit

---
+++ src/main/java/com/example/authoritymanagementsystem/model/Role.java
@@ -0,0 +1,21 @@
+package com.example.authoritymanagementsystem.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
---
+++ src/main/java/com/example/authoritymanagementsystem/model/User.java
@@ -0,0 +1,27 @@
+package com.example.authoritymanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+}
